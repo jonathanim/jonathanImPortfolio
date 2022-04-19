5b600516e4acfd35b8b6e894687475904fe83c3a
jonathanim <imjonathan.work@gmail.com>
2022-04-18 18:47:26 -0700

    creating layout and basic components

---
+++ src/components/Main.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import MyNavbar from './MyNavbar'
+
+function Main() {
+    return (
+        <MyNavbar />
+    )
+}
+
+export default Main
---
+++ src/components/MyNavbar.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Navbar, Container, Nav } from 'react-bootstrap'
+
+
+const MyNavbar = () => {
+    return (
+        <Navbar bg="dark" variant="dark">
+            <Container>
+                <Navbar.Brand href="#home">Jonathan Im</Navbar.Brand>
+                <Nav className="mx-auto">
+                    <Nav.Link href="#about">About</Nav.Link>
+                    <Nav.Link href="#projects">Projects</Nav.Link>
+                    <Nav.Link href="#contact">Contact</Nav.Link>
+                </Nav>
+            </Container>
+        </Navbar>
+    )
+}
+
+export default MyNavbar
