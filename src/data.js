a63f6b390612c1c05258db34538117532c8a1a38
jonathanim <imjonathan.work@gmail.com>
2022-05-30 12:11:52 -0700

    added ui project

--- src/data.js
+++ src/data.js
@@ -3,6 +3,9 @@ import gymapp from './public/gymapp.png'
 import phovina from './public/phovina.png'
 import animeSearch from './public/animeSearch.png'
 import portfolio from './public/Portfolio-practice.png'
+import valorant from './public/valorant.png'
+import reebok from './public/reebok.png'
+import ui from './public/ui.png'
 
 export const projectsData = [
     {
@@ -11,20 +14,18 @@ export const projectsData = [
         stack: "React,Pure CSS, Fetch, Async",
         url: "https://anime-mansion-db.netlify.app/",
         image: animeSearch
-    },
-    {
-        name: "Coffee Shop",
-        description: "Coffee restaurant mock up page",
-        stack: "HTML, CSS",
-        url: "https://coffeeshop-gg.netlify.app/",
-        image: coffee
-    },
-    {
-        name: "Phovina Restaurant",
-        description: "Restaurant website for Phovina.",
-        stack: "React, Bootstrap",
-        url: "https://phovina-anaheim.netlify.app/",
-        image: phovina
+    }, {
+        name: "ValoStart",
+        description: "Valorant information about maps, characters, weapons.",
+        stack: "React, TailwindCSS, react-router-dom, Axios",
+        url: "https://i-valo.netlify.app/",
+        image: valorant
+    }, {
+        name: "UI Cloud",
+        description: "UI Practice with TailwindCSS grids",
+        stack: "React, TailwindCSS, react-scroll",
+        url: "https://ui-practice.netlify.app/",
+        image: ui
     },
     {
         name: "Portfolio Practice",
@@ -34,12 +35,33 @@ export const projectsData = [
         image: portfolio
     },
     {
+        name: "Coffee Shop",
+        description: "Coffee restaurant mock up page.",
+        stack: "HTML, CSS",
+        url: "https://coffeeshop-gg.netlify.app/",
+        image: coffee
+    }, {
+        name: "Reebok HTML Email",
+        description: "HTML Email for email development",
+        stack: "HTML, CSS",
+        url: "https://reebok-email-test.netlify.app",
+        image: reebok
+    },
+    {
+        name: "Phovina Restaurant",
+        description: "Restaurant website for Phovina.",
+        stack: "React, Bootstrap",
+        url: "https://phovina-anaheim.netlify.app/",
+        image: phovina
+    },
+    {
         name: "Gym Mobile App",
         description: "App made with React Native.",
         stack: "React Native, Android Studio",
         url: "https://www.youtube.com/watch?v=MqRS5nS00K4&ab_channel=JonathanIm",
         image: gymapp
     },
+
 
 
 
